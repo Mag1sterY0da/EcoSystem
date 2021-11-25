9827f4ec34190aafc39e90c9b84ed42ff438e170
Sander <sashabro2002@gmail.com>
2021-11-26 00:19:58 +0200

    Added Entities to DAL

--- Catalog/Entities/Enterprice.cs
+++ Catalog/Entities/Enterprice.cs
@@ -8,7 +8,7 @@ namespace DAL.Entities
 {
     public class Enterprice
     {
-        public int EnterpriceID { get; set; }
+        public int EnterpriceId { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string Director { get; set; }
