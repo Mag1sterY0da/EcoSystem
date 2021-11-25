76287a8f1aaac87edcf8b2b3ec8617ebc2d3980b
Sander <sashabro2002@gmail.com>
2021-11-26 00:18:16 +0200

    Added Dal Project

---
+++ Catalog/Entities/Rec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Rec
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public Boolean Pollution { get; set; }
+        public string Pollution_type { get; set; }
+        public int Pollution_metrics { get; set; }
+
+    }
+}
